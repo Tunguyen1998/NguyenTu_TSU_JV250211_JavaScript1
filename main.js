331eab742fa34bb0b53429f4a46b772c7e82a846
Tunguyen1998 <102499154+Tunguyen1998@users.noreply.github.com>
2025-03-09 19:03:17 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-alert("Hello World !")
+let i = 10 ;// So nguyen
+let f = 20.5; // So thuc
+let b = true; // Kieu logic
+let s = "Ha Noi"; // Kieu chuoi
 
-let loiChao = 'Hello World !';
-console.log(loiChao)
-
-document.write("Hello World !");
+document.write("Bien i co gia tri la : " + i + "<br>");
+document.write("Bien f co gia tri la : " + f + "<br>");
+document.write("Bien b co gia tri la : "+ b + "<br>");
+document.write("Bien s co gia tri la : " + s + "<br>");
