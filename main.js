0c86158b450901599d06e87df1bd6846f91da5ac
Tunguyen1998 <102499154+Tunguyen1998@users.noreply.github.com>
2025-03-09 19:20:37 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-alert("Hello World !")
+let a = "Welcome to Rikkei Academy.";
+console.log(a);
 
-let loiChao = 'Hello World !';
-console.log(loiChao)
+document.write("Quyet tam hoc lap trinh JavaScript !!!");
 
-document.write("Hello World !");
+alert(" Chao mung ban den voi hoc vien Rikkei Academy !!!");
+
+let b = confirm ( " Ban da du 18 tuoi chua ?\ Nhấn OK để chọn 'Yes', Cancel để chọn 'No'");
+prompt
